9b8d3705b8cf69f4d2e3e39c89424231991b8c36
Ben Livermore <blivermore@wespire.com>
2020-06-07 11:50:52 -0400

    Add files via upload

--- notes-app.js
+++ notes-app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const uuid = require('uuidv4').uuid;
 const app = express();
 const port = process.env.PORT || 3000;
 
+function createNote(noteText) {
+  return {
+    id: uuid(),
+    text: noteText
+  };
+}
+
 const notes = [
-  'http is a protocol',
-  'http requests have a url, method, header, and body',
-  'http methods used in forms or GET or POST',
-  'http methods PUT and DELETE cannot be used in forms'
+  createNote('http is a protocol'),
+  createNote('http requests have a url, method, header, and body'),
+  createNote('http methods used in forms or GET or POST'),
+  createNote('http methods PUT and DELETE cannot be used in forms')
 ];
 
 app.set('view engine', 'ejs');
@@ -25,17 +32,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/delete-note/:noteId', (req, res) => {
-  notes.splice(req.params.noteId, 1)
+  const noteToDeleteIndex = notes.findIndex(note => note.id === req.params.noteId)
+  notes.splice(noteToDeleteIndex, 1);
   res.redirect('/');
 });
 
 app.post('/notes/:noteId', (req, res) => {
-  notes[req.params.noteId] = req.body.note;
+  const noteToUpdate = notes.find(note => note.id === req.params.noteId)
+  noteToUpdate.text = req.body.note;
   res.redirect('/');
 });
 
 app.post('/notes', (req, res) => {
-  notes.push(req.body.note);
+  const newNote = createNote(req.body.note);
+  notes.push(newNote);
   res.redirect('/');
 });
 
